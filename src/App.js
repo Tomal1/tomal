841dc654e494552ddd82cd096b3afcc179c7728e
Minhazul Islam <minhazulislam@hotmail.com>
2023-04-13 21:03:24 +0100

    components have been added to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import Home from "./Sections/Home";
+import Nav from "./Components/Nav";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <main className="main">
+      <Nav/>
+      <div className="app_section_container">
+        <Home/>
+      </div>
+      <Contact/>
+      <Footer/>
+    </main>
 
   );
 }
