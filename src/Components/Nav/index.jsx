958d3a7738e55d07b1b08f36621b0e7b0f99d9f9
Minhazul Islam <minhazulislam@hotmail.com>
2023-04-14 09:00:38 +0100

    About and home section also resume button now downloads a cv

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,28 +1,33 @@
 import styles from "./style.module.css";
+import resume from "../../Assets/resume.pdf";
 
-const Nav = () =>{
-    return (
-      <nav className={styles.nav_container}>
-        <div className={styles.nav_left}>
-          <a href="#home">Tomal</a>
-        </div>
-        <div class={styles.nave_right}>
-          <a href="#about" className={styles.nav_link}>
-            <span>01</span>About
+const Nav = () => {
+  return (
+    <nav className={styles.nav_container}>
+      <div className={styles.nav_left}>
+        <a href="#home">Tomal</a>
+      </div>
+      <div class={styles.nave_right}>
+        <a href="#about" className={styles.nav_link}>
+          <span>01</span>About
+        </a>
+        <a href="#experience" className={styles.nav_link}>
+          <span>02</span>Experience
+        </a>
+        <a href="#work" className={styles.nav_link}>
+          <span>03</span>Work
+        </a>
+        <a href="#contact" className={styles.nav_link}>
+          <span>04</span>Contact
+        </a>
+        <button className={styles.nav_resume_btn}>
+          <a href={resume} download>
+            Resume
           </a>
-          <a href="#experience" className={styles.nav_link}>
-            <span>02</span>Experience
-          </a>
-          <a href="#work" className={styles.nav_link}>
-            <span>03</span>Work
-          </a>
-          <a href="#contact" className={styles.nav_link}>
-            <span>04</span>Contact
-          </a>
-          <button className={styles.nav_resume_btn}>Resume</button>
-        </div>
-      </nav>
-    );
-}
+        </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
